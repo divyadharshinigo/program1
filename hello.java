cfb276897739bb368bf71d13b9db2bba571b0955
divyadharshinigomathinayagam <dd.nayagam95@gmail.com>
2016-09-11 16:47:27 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+public static void main(String args[])
+{
+int i,n;
+System.out.print("ENTER NUMBER of times to repeat the string");
+n=sc.nextInt();
+for(i=0;i<=n;i++)
+{
+System.out.println("HELLO");
+
+}
+}
+}
+
